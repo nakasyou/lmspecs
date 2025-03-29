548e1977c50fbe7f255901e3867aeb79643e9cc4
nakasyou <syoutarou1103@outlook.jp>
2025-03-29 14:40:15 +0900

    feat: better transition

--- web/App.tsx
+++ web/App.tsx
@@ -2,23 +2,30 @@ import './App.css'
 import { lazy } from 'solid-js'
 import { Route, Router } from '@solidjs/router'
 import { Suspense } from 'solid-js'
+import Spinner from './components/Spinner.tsx'
+import { JSX } from 'solid-js'
 
 const Home = lazy(() => import('./pages/home/index.tsx'))
 const Chart = lazy(() => import('./pages/chart.tsx'))
 const Model = lazy(() => import('./pages/model/model-page.tsx'))
 const ModelList = lazy(() => import('./pages/model/index.tsx'))
 
+function wrapPageForLoading(Component: () => JSX.Element) {
+  return () => {
+    return <Suspense fallback={<div class='h-dvh grid place-items-center'><Spinner /></div>}>
+      <Component />
+    </Suspense>
+  }
+}
 export default function App(props: {
   url?: string
 }) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Router url={props.url}>
-        <Route path='/' component={Home} />
-        <Route path='/chart' component={Chart} />
-        <Route path='/model/:modelId' component={Model} />
-        <Route path='/model' component={ModelList} />
-      </Router>
-    </Suspense>
+    <Router url={props.url}>
+      <Route path='/' component={wrapPageForLoading(Home)} />
+      <Route path='/chart' component={wrapPageForLoading(Chart)} />
+      <Route path='/model/:modelId' component={wrapPageForLoading(Model)} />
+      <Route path='/model' component={wrapPageForLoading(ModelList)} />
+    </Router>
   )
 }
